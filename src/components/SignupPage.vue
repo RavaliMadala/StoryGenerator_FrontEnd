5ee4c0bb156beafb399e3e74920e9661f7bc9177
Srikanthbussa1 <srikanth.bussa@eagles.oc.edu>
2024-05-30 14:33:49 -0500

    Signup page V2-srikanth

--- src/components/SignupPage.vue
+++ src/components/SignupPage.vue
@@ -1,75 +1,147 @@
 <template>
-  <v-sheet>
+  <br/>
+  <v-sheet
+    class="pa-4 text-center mx-auto"
+    elevation="12"
+    max-width="600"
+    rounded="lg"
+    width="100%"
+  >
     <h1 class="text-center">Register</h1>
-    <br/>
-    <v-card class="mx-auto px-6 py-8" max-width="344">
-      <v-form
-        v-model="form"
-        @submit.prevent="onSubmit"
-      >
-        <v-text-field
-          v-model="username"
-          :readonly="loading"
-          :rules="[required]"
-          class="mb-2"
-          clearable
-          label="Username"
-        ></v-text-field>
 
-        <v-text-field
-          v-model="password"
-          :readonly="loading"
-          :rules="[required]"
-          clearable
-          label="Password"
-          placeholder="Enter your password"
-          onch
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show1 ? 'text' : 'password'"
-          @click:append="show1 = !show1"
-        ></v-text-field>
-
-        <v-text-field
-          v-model="passwordConfirm"
-          :readonly="loading"
-          :rules="[required]"
-          clearable
-          label="Confirm Password"
-          placeholder="Confirm password"
-          :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show2 ? 'text' : 'password'"
-          @click:append="show2 = !show2"
-        ></v-text-field>
-
-        <v-alert
-          variant="outlined"
-          type="warning"
-          prominent
-          border="top"
-          value="passwordAlert"
-          v-show="showAlert"
+    <v-container>
+      <v-card class="mx-auto" max-width="500" elevation="0">
+        <v-form
+          v-model="form"
+          @submit.prevent="onSubmit"
         >
-          {{alertMessage}}
-        </v-alert>
-        
-        <br/>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="email"
+                :readonly="loading"
+                :rules="emailRules"
+                class="mb-2"
+                clearable
+                label="Email"
+                hide-details
+              ></v-text-field>
+            </v-col>
+          </v-row>
 
-        <v-btn
-          :disabled="!form"
-          :loading="loading"
-          block
-          color="#711429"
-          size="large"
-          type="submit"
-          variant="elevated"
-          rounded="xl"
-          @click="registerClick"
-        >
-          Register
-        </v-btn>
-        <br/>
-      </v-form>
-    </v-card>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="firstName"
+                :readonly="loading"
+                :rules="[required]"
+                class="mb-1"
+                clearable
+                label="First Name"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="6">
+              <v-text-field
+                v-model="lastName"
+                :readonly="loading"
+                :rules="[required]"
+                class="mb-1"
+                clearable
+                label="Last Name"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="password"
+                :readonly="loading"
+                :rules="[required]"
+                clearable
+                label="Password"
+                placeholder="Enter your password"
+                onch
+                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show1 ? 'text' : 'password'"
+                @click:append="show1 = !show1"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="passwordConfirm"
+                :readonly="loading"
+                :rules="[required]"
+                clearable
+                label="Confirm Password"
+                placeholder="Confirm password"
+                :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show2 ? 'text' : 'password'"
+                @click:append="show2 = !show2"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="phoneNumber"
+                :readonly="loading"
+                :rules="phoneNumberRules"
+                class="mb-2"
+                clearable
+                label="Phone Number"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="12">
+              <v-select
+                v-model="gender"
+                :items="genderItem"
+                :rules="[v => !!v || 'Item is required']"
+                label="Gender"
+                required
+              ></v-select>
+            </v-col>
+          </v-row>
+
+          <v-alert
+            variant="outlined"
+            type="warning"
+            prominent
+            border="top"
+            value="passwordAlert"
+            v-show="showAlert"
+          >
+            {{alertMessage}}
+          </v-alert>
+            
+          <br/>
+
+          <v-btn
+            :disabled="!form"
+            :loading="loading"
+            block
+            color="#711429"
+            size="large"
+            type="submit"
+            variant="elevated"
+            rounded="xl"
+            @click="registerClick"
+          >
+            Register
+          </v-btn>
+          <br/>
+        </v-form>
+      </v-card>
+
+    </v-container>
 
     <v-snackbar
       v-model="snackbar"
@@ -88,9 +160,14 @@ import AuthenticationService from '@/services/UserAuthenticationService'
   export default {
     data: () => ({
       form: false,
-      username: null,
+      email: null,
+      firstName: null,
+      lastName: null,
       password: null,
       passwordConfirm: null,
+      phoneNumber: null,
+      gender: null,
+      genderItem: ['Male','Female','Other'],
       loading: false,
       showAlert: false,
       alertMessage: "",
@@ -98,6 +175,30 @@ import AuthenticationService from '@/services/UserAuthenticationService'
       timeout: 2000,
       show1: false,
       show2: false,
+      emailRules: [
+        value => {
+          if (value) return true
+
+          return 'E-mail is requred.'
+        },
+        value => {
+          if (/.+@.+\..+/.test(value)) return true
+
+          return 'E-mail must be valid.'
+        },
+      ],
+      phoneNumberRules: [
+        value => {
+          if (value) return true
+
+          return 'Phone Number is requred.'
+        },
+        value => {
+          if (value?.length == 9 && /[0-9-]+/.test(value)) return true
+          
+          return 'Phone Number must be valid.'
+        },
+      ]
     }),
 
     methods: {
@@ -114,25 +215,19 @@ import AuthenticationService from '@/services/UserAuthenticationService'
       async registerClick(){
         try{
           await AuthenticationService.register({
-              username: this.username,
+              email: this.email,
+              firstName: this.firstName,
+              lastName: this.lastName,
+              phoneNumber: this.phoneNumber,
+              gender: this.gender,
               password: this.password,
               permission: "user"
           }).then((response)=> {
                 console.log(response)
                 if(response.statusText == "OK"){
-                  this.$store.commit('setUserName', response.data.username)
-                  this.$store.commit('setPermission', response.data.permission)
-                  this.$store.commit('setIsUserLoggedIn', true)
-
-                  sessionStorage.setItem('UserName', response.data.username)
-                  sessionStorage.setItem('UserPermission', response.data.permission)
-                  sessionStorage.setItem('IsUserLoggedIn', true)
-                  console.log(sessionStorage.getItem('UserName'))
-                  console.log(sessionStorage.getItem('UserPermission'))
-                  console.log(sessionStorage.getItem('IsUserLoggedIn'))
                   this.clearFields()
                   this.snackbar = true
-                  setTimeout(() => (router.push('/')), 2000)
+                  setTimeout(() => (router.push('/login')), 1000)
                 }
               }
           )
@@ -144,8 +239,12 @@ import AuthenticationService from '@/services/UserAuthenticationService'
       },
       clearFields(){
         this.password = ""
-        this.username = ""
+        this.email = ""
         this.passwordConfirm = ""
+        this.firstName = ""
+        this.lastName = ""
+        this.phoneNumber = ""
+        this.gender = ""
       },
       showError(errorText){
         this.alertMessage = errorText;
