dae8c0e6e36ec57314aa99990916296bc7111ed2
Ravali Madala <r.madala@eagles.oc.edu>
2024-06-06 15:20:30 -0500

    StoryList API

--- src/services/StoryService.js
+++ src/services/StoryService.js
@@ -3,6 +3,9 @@ import Api from '@/services/Api'
 export default{
     saveStory (request) {
         return Api().post('/saveStory', request)
+    },
+    getStories (userID){
+        return Api().get('/Stories/' + userID)
     }
 }
 
